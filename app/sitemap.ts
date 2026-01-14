bf89729da2179dee30d1ddd9fda9984f1c57abe6
mukslima <marcos.nascimento.nl@gmail.com>
2026-01-14 10:36:30 -0300

    Sitemap SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,12 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://www.suporteoxi.com.br/",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
   ];
 }
